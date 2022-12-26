bd314d3a377e9868c1147c586284bf62245d2c1c
hwuiinn <gnldls8406@gmail.com>
2022-12-26 20:57:18 +0900

    #2 design fix file
    파일 충돌로 인해 제대로 올라가지 않은 파일들을 다시 올림.

--- src/components/BoardColumn.jsx
+++ src/components/BoardColumn.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getBoardThunk } from "../redux/modules/boardSlice";
 import styled from "styled-components";
 import BoardItem from "./BoardItem";
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 import { nanoid } from "@reduxjs/toolkit";
 
@@ -15,19 +16,28 @@ const BoardColumnStyled = styled.div`
   padding: 0.625rem;
   padding-bottom: 1.875rem;
 =======
+=======
+import { nanoid } from "@reduxjs/toolkit";
+>>>>>>> Stashed changes
 
 const BoardColumnStyled = styled.div`
   max-width: 21.25rem;
-  min-height: 50vh;
   width: 100%;
   height: 100%;
 
+<<<<<<< Updated upstream
   margin: 0 0.3125rem;
 >>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
+=======
+  margin: 0 0.625rem;
+  padding: 0.625rem;
+  padding-bottom: 1.875rem;
+>>>>>>> Stashed changes
 
   border: 1px solid #ccc;
   border-radius: 0.625rem;
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   box-shadow: 0 0 3px rgba(170, 170, 170, 1);
 
@@ -37,12 +47,19 @@ const BoardColumnStyled = styled.div`
     box-shadow: 0.3125rem 0.3125rem 0.75rem rgba(0, 0, 0, 0.7);
 =======
   box-shadow: 0 0 5px rgba(170, 170, 170, 1);
+=======
+  box-shadow: 0 0 3px rgba(170, 170, 170, 1);
+>>>>>>> Stashed changes
 
-  transition-duration: 0.2s;
+  transition-duration: 0.3s;
 
   &:hover {
+<<<<<<< Updated upstream
     box-shadow: 0.3125rem -0.3125rem 0.375rem rgba(0, 0, 0, 0.3);
 >>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
+=======
+    box-shadow: 0.3125rem 0.3125rem 0.75rem rgba(0, 0, 0, 0.7);
+>>>>>>> Stashed changes
   }
 
   /* background-color: beige; */
@@ -71,6 +88,7 @@ const BoardColumn = (props) => {
     dispatch(getBoardThunk());
   }, [dispatch]);
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   //  <BoardColumn category="Schedule" /> prop으로 내려운 category와 db.json의 category와 매칭시켜서 kanban의 위치를 지정함..
   const renderedBoardItems = boards.map((item) => {
@@ -82,12 +100,20 @@ const BoardColumn = (props) => {
     return null;
 =======
   //  <BoardColumn title="Schedule" /> title을 db.json의 category와 매칭시켜서 kanban의 위치를 지정함..
+=======
+  //  <BoardColumn category="Schedule" /> prop으로 내려운 category와 db.json의 category와 매칭시켜서 kanban의 위치를 지정함..
+>>>>>>> Stashed changes
   const renderedBoardItems = boards.map((item) => {
     console.log("item.category : ", item.category);
+
     if (item.category === props.category) {
-      return <BoardItem {...item}></BoardItem>;
+      return <BoardItem {...item} key={nanoid()}></BoardItem>;
     }
+<<<<<<< Updated upstream
 >>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
+=======
+    return null;
+>>>>>>> Stashed changes
   });
 
   return (
--- src/components/BoardItem.jsx
+++ src/components/BoardItem.jsx
@@ -14,6 +14,14 @@ const BoardItemBox = styled.div`
   margin: 0 auto;
   margin-bottom: 1.5625rem;
   padding: 0.625rem 1.25rem 1.25rem 1.25rem;
+<<<<<<< Updated upstream
+=======
+
+  transition-duration: 0.3s;
+  :hover {
+    border: 0.1875rem solid #2563eb;
+  }
+>>>>>>> Stashed changes
 
 <<<<<<< HEAD
   transition-duration: 0.3s;
@@ -40,21 +48,53 @@ const BoardItemBox = styled.div`
   .boardTop {
     display: flex;
     justify-content: space-between;
+    align-items: center;
+
+    margin-bottom: -0.625rem;
   }
 
   h2 {
-    background-color: aqua;
-    min-height: 4.375rem;
+    margin-bottom: 0.625rem;
+    font-size: 1.25rem;
   }
 
   .closeBtn {
+<<<<<<< Updated upstream
 >>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
+=======
+    padding: 0 7px;
+>>>>>>> Stashed changes
     border: 0rem;
     font-size: 1.85rem;
     transition-duration: 0.3s;
     :hover {
       color: #eb5a3d;
     }
+<<<<<<< Updated upstream
+  }
+
+  .detailBtn {
+    border: 0rem;
+    text-decoration: none;
+    color: black;
+  }
+
+  .manager {
+    font-size: 0.875rem;
+    font-weight: bold;
+  }
+
+  .content-box {
+    min-height: 100px;
+    height: 100%;
+
+    padding-top: 10px;
+    padding-left: 10px;
+
+    border: 1px solid #aaa;
+    border-radius: 10px;
+=======
+>>>>>>> Stashed changes
   }
 
   .detailBtn {
@@ -78,14 +118,9 @@ const BoardItemBox = styled.div`
     border: 1px solid #aaa;
     border-radius: 10px;
   }
-
-  .detailBtn {
-    border: 0rem;
-    text-decoration: none;
-    color: black;
-  }
 `;
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 const BoardItem = ({ name, id, title, category, content }) => {
 =======
@@ -104,6 +139,10 @@ const BoardItem = ({ name, id, title, category }) => {
   //   content: "보드 내용",
   //   pw: "보드 비밀번호",
   // };
+=======
+const BoardItem = ({ name, id, title, category, content }) => {
+  const dispatch = useDispatch();
+>>>>>>> Stashed changes
 
 >>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
   const onDelete = (id) => {
@@ -114,11 +153,15 @@ const BoardItem = ({ name, id, title, category }) => {
   return (
     <BoardItemBox>
       <div className="boardTop">
+<<<<<<< Updated upstream
 <<<<<<< HEAD
         <div className="manager"> 담당자 : {name} </div>
 =======
         <div> 담당자 : {name} </div>
 >>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
+=======
+        <div className="manager"> 담당자 : {name} </div>
+>>>>>>> Stashed changes
         <button
           className="closeBtn"
           onClick={() => {
@@ -130,14 +173,20 @@ const BoardItem = ({ name, id, title, category }) => {
 
       <Link className="detailBtn" to={id}>
         <h2> {title} </h2>
+<<<<<<< Updated upstream
 <<<<<<< HEAD
+=======
+>>>>>>> Stashed changes
         {/*보드 내용 삽입*/}
         <div className="content-box">
           <p>{content}</p>
         </div>
         {/**/}
+<<<<<<< Updated upstream
 =======
 >>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
+=======
+>>>>>>> Stashed changes
       </Link>
       <CustomBtn
         name="nextCategory"
