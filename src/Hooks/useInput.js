573c73eff745fea854f2e58741d3ee89c5e62694
arch-spatula <shkim516@naver.com>
2022-12-26 17:45:32 +0900

    Feat, custom hook useInput useCallback으로 최적화 #1

--- src/Hooks/useInput.js
+++ src/Hooks/useInput.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 /**
  * input type="text"와 textarea를 가정으로 사용하는 custom hook입니다.
@@ -9,10 +9,10 @@ const useInput = (initialText = "") => {
    * 제어된 input
    * @code `onChange={(e) => handleOnChange(e)}`
    */
-  const handleOnChange = (e) => setInputValue(e.target.value);
+  const handleOnChange = useCallback((e) => setInputValue(e.target.value), []);
 
   // 리셋 함수
-  const resetInput = setInputValue("");
+  const resetInput = useCallback(() => setInputValue(""), []);
 
   return [inputValue, handleOnChange, resetInput];
 };
