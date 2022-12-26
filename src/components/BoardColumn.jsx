df15141ac27afad7f4528aad8b93288f53433989
hwuiinn <gnldls8406@gmail.com>
2022-12-26 21:01:07 +0900

    #2 design fix file
    파일 충돌로 인해 제대로 올라가지 않은 파일들을 다시 올림.

--- src/components/BoardColumn.jsx
+++ src/components/BoardColumn.jsx
@@ -3,8 +3,6 @@ import { useSelector, useDispatch } from "react-redux";
 import { getBoardThunk } from "../redux/modules/boardSlice";
 import styled from "styled-components";
 import BoardItem from "./BoardItem";
-<<<<<<< Updated upstream
-<<<<<<< HEAD
 import { nanoid } from "@reduxjs/toolkit";
 
 const BoardColumnStyled = styled.div`
@@ -15,51 +13,16 @@ const BoardColumnStyled = styled.div`
   margin: 0 0.625rem;
   padding: 0.625rem;
   padding-bottom: 1.875rem;
-=======
-=======
-import { nanoid } from "@reduxjs/toolkit";
->>>>>>> Stashed changes
-
-const BoardColumnStyled = styled.div`
-  max-width: 21.25rem;
-  width: 100%;
-  height: 100%;
-
-<<<<<<< Updated upstream
-  margin: 0 0.3125rem;
->>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
-=======
-  margin: 0 0.625rem;
-  padding: 0.625rem;
-  padding-bottom: 1.875rem;
->>>>>>> Stashed changes
 
   border: 1px solid #ccc;
   border-radius: 0.625rem;
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
   box-shadow: 0 0 3px rgba(170, 170, 170, 1);
 
   transition-duration: 0.3s;
 
   &:hover {
     box-shadow: 0.3125rem 0.3125rem 0.75rem rgba(0, 0, 0, 0.7);
-=======
-  box-shadow: 0 0 5px rgba(170, 170, 170, 1);
-=======
-  box-shadow: 0 0 3px rgba(170, 170, 170, 1);
->>>>>>> Stashed changes
-
-  transition-duration: 0.3s;
-
-  &:hover {
-<<<<<<< Updated upstream
-    box-shadow: 0.3125rem -0.3125rem 0.375rem rgba(0, 0, 0, 0.3);
->>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
-=======
-    box-shadow: 0.3125rem 0.3125rem 0.75rem rgba(0, 0, 0, 0.7);
->>>>>>> Stashed changes
   }
 
   /* background-color: beige; */
@@ -88,8 +51,6 @@ const BoardColumn = (props) => {
     dispatch(getBoardThunk());
   }, [dispatch]);
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
   //  <BoardColumn category="Schedule" /> prop으로 내려운 category와 db.json의 category와 매칭시켜서 kanban의 위치를 지정함..
   const renderedBoardItems = boards.map((item) => {
     console.log("item.category : ", item.category);
@@ -98,22 +59,6 @@ const BoardColumn = (props) => {
       return <BoardItem {...item} key={nanoid()}></BoardItem>;
     }
     return null;
-=======
-  //  <BoardColumn title="Schedule" /> title을 db.json의 category와 매칭시켜서 kanban의 위치를 지정함..
-=======
-  //  <BoardColumn category="Schedule" /> prop으로 내려운 category와 db.json의 category와 매칭시켜서 kanban의 위치를 지정함..
->>>>>>> Stashed changes
-  const renderedBoardItems = boards.map((item) => {
-    console.log("item.category : ", item.category);
-
-    if (item.category === props.category) {
-      return <BoardItem {...item} key={nanoid()}></BoardItem>;
-    }
-<<<<<<< Updated upstream
->>>>>>> 1116bf55115514f4940b2cfbd9a0733cdfbaed12
-=======
-    return null;
->>>>>>> Stashed changes
   });
 
   return (
